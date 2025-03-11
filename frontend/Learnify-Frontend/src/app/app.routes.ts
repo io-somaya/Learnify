08905ddc2aed6a9c33ba89265a1e0c3f1e8b7ff3
OmarMMRizk <omar2znz61@gamil.com>
2025-03-12 00:26:30 +0200

    add route active  and example for lasyloading

--- frontend/Learnify-Frontend/src/app/app.routes.ts
+++ frontend/Learnify-Frontend/src/app/app.routes.ts
@@ -1,12 +1,32 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './admin/layout/layout.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: "dashboard", component: LayoutComponent,title: "Dashboard", 
+//Admin routes 
+    // {
+    //     path:"admin/login",
+    //     component: LoginComponent,
+    //     title: "Login Admin"
+    // },
+
+    {path: "admin/dashboard", component: LayoutComponent,title: "Dashboard", 
         children: [{
             path: "",
             component:DashboardComponent,
-            title: "Dashboard"
-        }]}
+            //loadComponet use to make lasyloading we can 
+            // loadComponent:()=>import("./admin/dashboard/dashboard.component").then((C)=> C.DashboardComponent),
+            title:"Dashboard"        
+        },  
+        ]
+    },
+    
+// User routes
+
+
+
+//Other routes
+
+    
 ];
