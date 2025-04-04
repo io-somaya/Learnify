da91030e27d040c1cf8108242160fbd0b8f85cac
OmarMMRizk <omar2znz61@gamil.com>
2025-04-04 07:56:18 +0200

    feat(packages): Add route for PackagesComponent and update package service response handling

--- frontend/learnify-frontend/src/app/services/package.service.ts
+++ frontend/learnify-frontend/src/app/services/package.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError , tap} from 'rxjs/operators';
 import { environment } from '../../.environments/environment';
 import { IPackage } from '../Interfaces/IPackage';
+import { catchError, tap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +15,15 @@ export class PackageService {
   constructor(private http: HttpClient) {}
 
   getPackages(): Observable<IPackage[]> {
-    return this.http.get<IPackage[]>(`${this.apiUrl}/packages`).pipe(
-      tap((packages: IPackage[]) => {
-        // this.packages = packages;
-        console.log('Fetched packages:', packages);
+    return this.http.get<{ data: IPackage[] }>(`${this.apiUrl}/packages`).pipe(
+      tap((res) => {
+        console.log('Fetched packages:', res.data);
       }),
+      map((res) => res.data),
       catchError(this.handleError)
     );
   }
+  
 
   private handleError(error: HttpErrorResponse) {
     let errorMessage = 'An unknown error occurred!';
