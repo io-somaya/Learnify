5108da7c751300c5bc42a72a82db2facdb21054f
Abdo-hasen <abdohasen200@gmail.com>
2025-04-17 15:45:44 +0200

    feat: Update AssignmentController to include lesson and questions count in index; implement show method for single assignment retrieval

--- backend/learnify-backend/app/Http/Controllers/Api/Teacher/AssignmentController.php
+++ backend/learnify-backend/app/Http/Controllers/Api/Teacher/AssignmentController.php
@@ -10,39 +10,45 @@ use App\Http\Resources\SubmissionResource;
 use App\Services\AssignmentService;
 use App\Http\traits\ApiTrait;
 use App\Models\Assignment;
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Exception; 
-use Illuminate\Database\Eloquent\ModelNotFoundException; 
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AssignmentController extends Controller
 {
-    use ApiTrait; 
+    use ApiTrait;
 
-    protected AssignmentService $assignmentService; // Dependency Injection
-
-    /**
-     * Constructor to inject the AssignmentService.
-     */
-    public function __construct(AssignmentService $assignmentService)
-    {
-        $this->assignmentService = $assignmentService;
-    }
 
     /**
-     * Display a listing of the assignments.
-     * GET /api/teacher/assignments
+     * Display a listing of the assignments with their related lesson and questions count.
      *
      * @return JsonResponse
      */
     public function index(): JsonResponse
     {
-        // Eager load lesson and count questions efficiently
-        $assignments = Assignment::with('lesson','questions') 
-                                ->latest()
-                                ->get();
+        // Eager load lesson and count questions 
+        $assignments = Assignment::with('lesson', 'questions')
+            ->latest()
+            ->get();
 
         $data = AssignmentResource::collection($assignments);
-        return $this->apiResponse(200, 'Assignments retrieved successfully.', null, $data);
+        return $this->apiResponse(200, 'Assignments with related lesson and questions count retrieved successfully.', null, $data);
     }
+
+
+    // Get single assignment with paginated questions
+    public function show(Assignment $assignment, Request $request)
+    {
+        $perPage = $request->get('per_page', 10); // Default to 10 questions per page
+        $assignment->load(['lesson']);
+        $questions = $assignment->questions()->with('options')->paginate($perPage);
+
+        return $this->apiResponse(200, 'Assignment retrieved with questions and their options Successfullt', null, [
+            'assignment' => new AssignmentResource($assignment),
+            'questions' => $questions,
+        ]);
+    }
+
 }
