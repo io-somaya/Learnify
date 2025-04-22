7de70e01709bc0911303d80aa9b1c9746b2b97f4
OmarMMRizk <omar2znz61@gamil.com>
2025-04-22 22:09:36 +0200

    fix the popup and add some style to Assignment(detail-subbmission detail-start)

--- frontend/learnify-frontend/src/app/student/assignment/result-popup/result-popup.component.ts
+++ frontend/learnify-frontend/src/app/student/assignment/result-popup/result-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,9 +8,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './result-popup.component.html',
   styleUrl: './result-popup.component.css'
 })
-export class ResultPopupComponent {
-  @Input() result: any;
+export class ResultPopupComponent implements OnInit {
+  @Input() submissionResult: any;
   @Output() close = new EventEmitter<void>();
+  
+  ngOnInit() {
+    console.log('Submission Result received:', this.submissionResult);
+  }
+
+  getResultData() {
+    // Handle different possible structures of the response
+    if (this.submissionResult?.data?.correct_answers !== undefined) {
+      return this.submissionResult.data;
+    } 
+    // For when the data property itself contains the metrics
+    else if (this.submissionResult?.correct_answers !== undefined) {
+      return this.submissionResult;
+    }
+    
+    return null;
+  }
 
   onClose() {
     this.close.emit();
