b086dd0633957aa5895397aecbe892bf7db0292a
OmarMMRizk <omar2znz61@gamil.com>
2025-04-24 18:55:45 +0200

    Student managemnts u can active and in active and change role and delete student and change grade

--- frontend/learnify-frontend/src/app/Interfaces/IUserProfile.ts
+++ frontend/learnify-frontend/src/app/Interfaces/IUserProfile.ts
@@ -13,3 +13,23 @@ export interface IUserProfile {
   created_at: string;
   updated_at: string;
 }
+
+export interface IPaginatedUsers {
+  current_page: number;
+  data: IUserProfile[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: Array<{
+    url: string | null;
+    label: string;
+    active: boolean;
+  }>;
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+}
--- frontend/learnify-frontend/src/app/app.routes.ts
+++ frontend/learnify-frontend/src/app/app.routes.ts
@@ -55,6 +55,8 @@ import { StartAssignmentComponent } from './student/assignment/start-assignment/
 import { AssignmentListComponent } from './student/assignment/assignment-list/assignment-list.component';
 import { SubmissionsAssignmentListComponent } from './student/assignment/submissions-assignment-list/submissions-assignment-list.component';
 import { AssignmemtSubmissionDetailComponent } from './student/assignment/assignmemt-submission-detail/assignmemt-submission-detail.component';
+import { StudentManagementComponent } from './admin/student-management/student-management.component';
+import { StudentDetailComponent } from './admin/student-management/student-detail/student-detail.component';
 
 export function route(name: string): string {
   const routes = {
@@ -233,6 +235,16 @@ export const routes: Routes = [
       path: 'assignments-management/submissions/:id',
       component: AssignmentSubmissionsComponent,
       title: 'Assignment Submissions'
+    },
+    {
+      path:"student-management",
+      component:StudentManagementComponent,
+      title:"Student Management"
+    },
+    {
+      path:"student-management/:id",
+      component:StudentDetailComponent,
+      title:"Student Management"
     },
     // Admin Profile Routes
     {
