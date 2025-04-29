4c336aaeb1d262768e6318d40f5743b3dfa4a698
little-smsm <somaiahassen1@gmail.com>
2025-04-29 08:56:42 +0300

    last changes

--- frontend/learnify-frontend/src/app/app.module.ts
+++ frontend/learnify-frontend/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { provideHttpClient, withInterceptors, HttpClientModule } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { AiAssistantModule } from './ai-assistant/ai-assistant.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [],
   imports: [BrowserModule, HttpClientModule, AiAssistantModule],
   providers: [
     provideHttpClient(withInterceptors([authInterceptor]))
   ],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
